330db6b933c60603953a08b46f24cf5902ff0e2d
BasnMyk <basnkonkolia6@gmail.com>
2025-08-15 15:31:11 +0300

    Update ga4_landing_pages_2020.sql

--- ga4_landing_pages_2020.sql
+++ ga4_landing_pages_2020.sql
@@ -1,38 +1,39 @@
 -- ga4_landing_pages_2020.sql
 -- Порівняння конверсії між різними посадковими сторінками (page path)
--- TODO: заміни `project.dataset.events_*` на свій шлях
-WITH base AS (
-  SELECT
-    user_pseudo_id,
-    (SELECT value.int_value FROM UNNEST(event_params) WHERE key = 'ga_session_id') AS session_id,
-    event_name,
-    (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'page_location') AS page_location,
-    TIMESTAMP_MICROS(event_timestamp) AS event_ts
-  FROM `project.dataset.events_*`
+With Hm_1 as (Select Replace( 
+(select value. string_value from Unnest(event_params) where key = 'page_location'),
+'https://shop.googlemerchandisestore.com/', ''
+) as page_path,
+Concat (
+   user_pseudo_id,
+   (select value. int_value from Unnest(event_params) where key = 'ga_session_id')  
+   ) as user_sessions_id,
+   event_name
+   FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
+WHERE _TABLE_SUFFIX BETWEEN '20200101' AND '20201231'
+  AND event_name IN ('session_start') 
+    ),
+Hm_2 as (
+  SELECT Distinct Concat (
+   user_pseudo_id,
+   (select value. int_value from Unnest(event_params) where key = 'ga_session_id')  
+   ) as user_sessions_id
+  FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
   WHERE _TABLE_SUFFIX BETWEEN '20200101' AND '20201231'
-    AND event_name IN ('session_start','purchase','page_view')
+    AND event_name = 'purchase'
 ),
-starts AS (
+Hm_3 as (
   SELECT
-    user_pseudo_id, session_id, event_ts,
-    REGEXP_EXTRACT(page_location, r'https?://[^/]+(/[^?#]*)') AS page_path
-  FROM base
-  WHERE event_name = 'session_start'
-),
-purchases AS (
-  SELECT DISTINCT user_pseudo_id, session_id
-  FROM base
-  WHERE event_name = 'purchase'
+    H_1.page_path,
+    H_1.user_sessions_id,
+  CASE WHEN H_2.user_sessions_id is not null then 1 else 0 end as purchase_1
+  FROM Hm_1 as H_1
+  LEFT join Hm_2 as H_2 ON H_1.user_sessions_id = H_2.user_sessions_id
 )
 SELECT
   page_path,
-  COUNT(DISTINCT CONCAT(s.user_pseudo_id, '-', s.session_id)) AS sessions_unique,
-  COUNT(DISTINCT IF(p.user_pseudo_id IS NOT NULL, CONCAT(s.user_pseudo_id, '-', s.session_id), NULL)) AS purchases_unique,
-  SAFE_DIVIDE(
-    COUNT(DISTINCT IF(p.user_pseudo_id IS NOT NULL, CONCAT(s.user_pseudo_id, '-', s.session_id), NULL)),
-    COUNT(DISTINCT CONCAT(s.user_pseudo_id, '-', s.session_id))
-  ) AS visit_to_purchase
-FROM starts s
-LEFT JOIN purchases p USING (user_pseudo_id, session_id)
-GROUP BY 1
-ORDER BY sessions_unique DESC;
+  COUNT(Distinct user_sessions_id) as sessions_start_count,
+  SUM(purchase_1) as purchase_count,
+  (SUM(purchase_1) / COUNT(Distinct user_sessions_id)) * 100 as visit_to_purchase
+FROM Hm_3
+Group by page_path;
